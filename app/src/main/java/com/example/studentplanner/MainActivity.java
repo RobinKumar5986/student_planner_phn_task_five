5b2b70966d3ec0c3e6a45b2d87b989f5d886e6b7
Robin Kumar <rkrobin6550@gmail.com>
2023-05-19 23:53:48 +0530

    Project in middle

--- app/src/main/java/com/example/studentplanner/MainActivity.java
+++ app/src/main/java/com/example/studentplanner/MainActivity.java
@@ -4,16 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    TextView welcome;
-
+    EditText signEmail;
+    EditText password;
+    Button SignIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        welcome=findViewById(R.id.textView);
+        signEmail=findViewById(R.id.txtUser1);
+        password=findViewById(R.id.txtPassword1);
+        SignIn=findViewById(R.id.btnSignIn);
+
+
+        SignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
